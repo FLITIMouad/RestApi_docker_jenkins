6e57fc92dbe14be641cf6826b2efaa05f3d5dd87
Mouad Fliti <asus16mouad@gmail.com>
2022-04-15 15:42:21 +0100

    first commit without dokerfile ...

---
+++ src/main/java/com/api/banque/BanqueServicesApplication.java
@@ -0,0 +1,13 @@
+package com.api.banque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/banque/BanqueServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.banque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqueServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
